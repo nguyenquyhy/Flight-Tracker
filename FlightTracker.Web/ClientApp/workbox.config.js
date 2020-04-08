c2f4669c30b5172c71f55c4d0831a69478d21eca
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-04-09 03:42:28 +0800

    Update packages and install craco workbox to customize blacklist

---
+++ FlightTracker.Web/ClientApp/workbox.config.js
@@ -0,0 +1,21 @@
+﻿module.exports = options => {
+    // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-webpack-plugin.GenerateSW#GenerateSW
+    options.skipWaiting = true;
+    options.navigateFallbackBlacklist = [
+        // Exclude URLs starting with /_, as they're likely an API call
+        new RegExp('^/_'),
+        // Exclude any URLs whose last part seems to be a file extension
+        // as they're likely a resource and not a SPA route.
+        // URLs containing a "?" character won't be blacklisted as they're likely
+        // a route with query params (e.g. auth callbacks).
+        new RegExp('/[^/?]+\\.[^/]+$'),
+        new RegExp('^/graphql', 'i'),
+        new RegExp('^/Error', 'i'),
+        new RegExp('^/Login/Google', 'i'),
+        new RegExp('^/Logout', 'i'),
+        new RegExp('^/signin-google', 'i'),
+        new RegExp('^/signout-google', 'i'),
+        new RegExp('^/signout-callback-google', 'i'),
+    ]
+    return options;
+};
